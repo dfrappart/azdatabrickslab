f506dec65c0d6a2dc24a1e2ec09eac960b209536
David Frappart <david.frappart-ext@rexel.com>
2021-08-05 23:31:47 +0200

    adding datasource to existing app reg

--- Terraformconfig/00_SubSetup/datasources.tf
+++ Terraformconfig/00_SubSetup/datasources.tf
@@ -5,3 +5,9 @@
 data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "currentclientconfig" {}
+
+# Referencing existing azure ad application registration
+
+data "azuread_application" "DTBSAADAppreg" {
+  display_name = var.DTBSAADAppreg
+}
