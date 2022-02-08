d350ec34f9b4672e6e6777f706e81640f0a3d890
David Frappart <david@frappart.com>
2022-02-08 21:32:49 +0100

    updating provider

--- Terraformconfig/02_databricks_secretmgmt/main.tf
+++ Terraformconfig/02_databricks_secretmgmt/main.tf
@@ -16,7 +16,7 @@ terraform {
 
 
 provider "azurerm" {
-  #subscription_id                          = var.AzureSubscriptionID
+  subscription_id                          = var.AzureSubscriptionID
   #client_id                                = var.AzureClientID
   #client_secret                            = var.AzureClientSecret
   #tenant_id                                = var.AzureTenantID
